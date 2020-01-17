0e59d2849d3f6d578cbadcf88bd3ef6bfd909f74
Rusah <Rusah@6467c47a-ad55-41f9-b700-838e7f817092>
2020-01-17 14:33:57 +0000

    Fidgeting
    git-svn-id: https://phoenixforge.cs.uchicago.edu/svn/rusah-cs152-win-20@6 6467c47a-ad55-41f9-b700-838e7f817092

--- lab2/evidence_lab2.c
+++ lab2/evidence_lab2.c
@@ -6,11 +6,89 @@
 void evidence_binary_char()
 {
     printf("*** testing binary_char\n");
-    printf("- expecting 00010001:", binary_char(17));
+    printf("- expecting 00010001:\n");
+    binary_char(17);
+    printf("\n");
+    printf("- expecting 00100111:\n");
+    binary_char(39);
+    printf("\n");
+	printf("- expecting 00000000:\n");
+	binary_char(0);
+	printf("\n");
+    printf("- expecting 11111111:\n");
+	binary_char(255);
+	printf("\n");
+	printf("- expecting 01101110: \n");
+	binary_char(110);
+	printf("\n");
 }
+
+void evidence_binary_int()
+{   
+    printf("*** testing binary_int\n");
+    printf("- expecting 17:\n");
+    binary_int(10001);
+    printf("\n");
+    printf("- expecting 39:\n");
+    binary_int(100111);
+    printf("\n"); 
+        printf("- expecting 0:\n");
+        binary_int(0000);
+        printf("\n");
+    printf("- expecting 255:\n");
+        binary_int(11111111);
+        printf("\n");
+        printf("- expecting 110: \n");
+        binary_int(11011110);
+        printf("\n");
+}
+
+void evidence_octal_char()
+{
+    printf("*** testing octal_char\n");
+    printf("- expecting 21:\n");
+    octal_char(17);
+    printf("\n");
+    printf("- expecting 47:\n");
+    octal_char(39);
+    printf("\n");
+        printf("- expecting 000:\n");
+        octal_char(0);
+        printf("\n");
+    printf("- expecting 377:\n");
+        octal_char(255);
+        printf("\n");
+        printf("- expecting 156: \n");
+        octal_char(110);
+        printf("\n");
+}
+
+void evidence_hex_char()
+{
+    printf("*** testing hex_char\n");
+    printf("- expecting 11:\n");
+    hex_char(17);
+    printf("\n");
+    printf("- expecting 27:\n");
+    hex_char(39);
+    printf("\n");
+    printf("- expecting 00:\n");
+    hex_char(0);
+    printf("\n");
+    printf("- expecting FF:\n");
+    hex_char(255);
+    printf("\n");
+    printf("- expecting D2: \n");
+    hex_char(110);
+    printf("\n");
+}
+
 
 int main(int argc, char *argv[]) 
 {
 	evidence_binary_char();
+	evidence_binary_int();
+	evidence_octal_char();
+	evidence_hex_char();
 	return 0;
 }
--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,19 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lab2.h"
 
-int aux_one(unsigned char byte, unsigned char length, unsigned char
-	binary_byte) {
-	 if (byte == 0) { 
-		return binary_byte;
-	} else if (byte % 2 == 0) {
-        return aux_one(byte / 2, length, 0); 
+void aux_one(int byte, int byte_two, char *binary[], int count) {
+	count += 1;
+	if (count == 8) {
+		printf("%s", *binary);
+	} else if (byte_two % 2 == 0) {
+		char *digit0 = "0";
+		binary[count] = digit0;
+		aux_one(byte, byte_two / 2, binary, count); 
     } else {
-        return aux_one(byte / 2, length, 1);
+		char *digit1 = "1";
+		binary[count] = digit1;
+        aux_one(byte, byte_two / 2, binary, count);
+    }
+}
+
+void aux_two(int byte, int byte_two) {
+    if (byte_two == 0) {
+        printf("");
+    } else if (byte_two % 8 != 0) {
+		int remain = (byte_two % 8);
+		printf("%d", remain);
+        aux_two(byte, byte_two / 8);
+    } else {
+        printf("0");
+        aux_two(byte, byte_two / 8);
+    }
+}
+
+void aux_three(int byte, int byte_two) {
+    if (byte_two == 0) {
+        printf("");
+    } else if (byte_two % 16 != 0) {
+        int remain = (byte_two % 16);
+		if (remain == 10) {
+			printf("A");
+		} else if (remain == 11) {
+			printf("B");
+		} else if (remain == 12) {
+			printf("C");
+		} else if (remain == 13) {
+			printf("D");
+		} else if (remain == 14) {
+			printf("E");
+		} else if (remain == 15) {
+			printf("F");
+		} else {
+			printf("%d", remain);
+        } 
+		aux_two(byte, byte_two / 16);
+    } else {
+        printf("0");
+        aux_two(byte, byte_two / 16);
     }
 }
 
 void binary_char(unsigned char byte) {
-	aux_one(17, 0, 1);
+	int add = 256;
+	int add_byte = byte;
+	int byte_two = add_byte + add;
+	aux_one(byte_two, byte_two, 00000000, 0);
+}
 
+void octal_char(unsigned char byte) {
+	int add = 511;
+	int add_byte = byte;
+	int byte_two = add_byte + add;
+	aux_two(byte, byte_two);
+}
+
+void hex_char(unsigned char byte) {
+    int add = 65535;
+    int add_byte = byte;
+    int byte_two = add_byte + add;
+    aux_three(byte, byte_two);
+}
+
+void int_aux1(unsigned int n, int dec, int mult) {
+    if (n == 0) {
+		printf("%d", dec);
+	} else if (n % 2 == 0) {
+		mult *= 2;
+		int_aux1(n, dec, mult);
+	} else {
+		n -= 1;
+		mult *= 2;
+		dec += mult;
+		int_aux1(n, dec, mult);
+	}
+}
+
+void binary_int(unsigned int n) {
+	int_aux1(n, 0, 1);
 }
