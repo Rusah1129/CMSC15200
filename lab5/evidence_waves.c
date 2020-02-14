3316cf7cdc0e6ad8a899f1b10b64edf0db890b1d
Rusah <Rusah@6467c47a-ad55-41f9-b700-838e7f817092>
2020-02-14 14:23:11 +0000

    lab5 finished and ready for submission
    git-svn-id: https://phoenixforge.cs.uchicago.edu/svn/rusah-cs152-win-20@31 6467c47a-ad55-41f9-b700-838e7f817092

---
+++ lab5/evidence_waves.c
@@ -0,0 +1,26 @@
+/* Ruhi Sah, rsah
+ * CS 152, Winter 2020
+ * Lab 5
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "waves.h"
+
+/* evidence_dist: test dist */
+void evidence_dist() {
+    printf("*** testing dist\n");
+    printf("- expecting 5 %f: \n", dist(3, 4, 0, 0));
+    printf("- expecting 5.099 %f: \n", dist(4, 3, 3, -2));
+    printf("- expecting 11.4018 %f: \n", dist(-10, 0, 1, 3));
+}
+
+/* main: run the evidence function above */
+int main(int argc, char *argv[])
+{
+  evidence_dist();
+  return 0;
+}
+
+
